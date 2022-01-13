7f8611e635a55bbeb7d657a23f83f78c94f75c80
mesreekanthh <mesreekanthh@gmail.com>
2022-01-13 13:27:46 +0530

    Committed

---
+++ src/main/java/com/abcd/abcd/MyController.java
@@ -0,0 +1,12 @@
+package com.abcd.abcd;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+@GetMapping(path="/")
+public String index() {
+return "HelloWorld";
+		}
+}
